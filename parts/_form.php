5a7c6a50c0b809cdb0dd6e893e4a8916073ce5a1
John Doe <johndoe@exemple.com>
2024-07-08 22:54:58 +0200

    store form data - form security

--- parts/_form.php
+++ parts/_form.php
@@ -3,39 +3,35 @@
     <div></div>
     <h2>Form</h2>
 
-    <form id="form" method="" action="post">
+    <form id="form" method="post" action="../database/treatment_form.php">
         <p>Please, leave a message here</p>
         <div>
             <div id="bodyForm">
                 <div id="left">
                     <div class="group">
-                        <label>Name of your agency</label>
-                        <input type="text" autocomplete="off" />
-                        <i class="fas fa-user"></i>
+                        <label for="agency">Name of your agency</label>
+                        <input type="text" id="agency" name="agency" placeholder="Example_58" autocomplete="off" pattern="[\w\d]{2,100}" required>
                     </div>
                     <div class="group">
-                        <label>Your name</label>
-                        <input type="text" autocomplete="off" />
-                        <i class="fas fa-user"></i>
+                        <label for="lastname">Your name</label>
+                        <input type="text" id="lastname" name="lastname" placeholder="John-Joe" autocomplete="off" pattern="[A-Za-z-]{2,100}" required>
                     </div>
                     <div class="group">
-                        <label>Your email</label>
-                        <input type="text" autocomplete="off" />
-                        <i class="fas fa-envelope"></i>
+                        <label for="email">Your email</label>
+                        <input type="email" id="email" name="email" placeholder="example@example.com" autocomplete="off" pattern="^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$" required>
                     </div>
                 </div>
 
                 <div id="right">
                     <div class="group">
-                        <label>Message</label>
-                        <textarea placeholder="Your message..."></textarea>
+                        <label for="contentMsg">Message</label>
+                        <textarea placeholder="Your message..." id="contentMsg" name="contentMsg" maxlength="500" required></textarea>
                     </div>
                 </div>
             </div>
         </div>
         <div class="footerForm" text-align="center">
-            <button>Envoyer le message</button>
+            <button type="submit" name="sendMsg">Envoyer le message</button>
         </div>
     </form>
 </section>
-
